2de84ecca3216a4ffb00137bf1192de37bb05df7
Omkar Gadam <omkargangadhargadam@gmail.com>
2024-10-01 04:05:11 +0530

    some changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,18 +20,19 @@ app.use(cors());
 app.use(express.json({limit:"40kb"}));
 app.use(express.urlencoded({limit:"40kb",extended:true}));
 app.use("/api/v1/users", userRoutes);
-
+//process.env.MONGO_URL
 
 const start = async () => {
-    try {
-        const connectionDb = await mongoose.connect(process.env.MONGO_URL);
-        console.log(`Mongo connected db: ${connectionDb.connection.host}`);
-        server.listen(app.get("port"), () => {
-            console.log("listening on port", app.get("port"));
-        });
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-    }
-};
+    app.set("mongo_user")
+    const connectionDb = await mongoose.connect(process.env.MONGO_URL)
+
+    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log("LISTENIN ON PORT 8000")
+    });
+
+
+
+}
 
 start();
